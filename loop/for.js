813b677d0bf1e7871a55b68137a6d6a4841d3b9c
404Shihab <shihabbhuiyan500@gmail.com>
2026-01-27 12:00:28 +0600

    for loop

---
+++ loop/for.js
@@ -0,0 +1,13 @@
+for (let i=1; i<=10; i++)
+{
+    console.log(i,"Bangladesh");
+}
+
+console.log("------------------------");
+
+const cars = ['volvo','honda','toyota','bmw','ford'];
+let len = cars.length;
+for(let i=0; i<len; i++)
+{
+    console.log(cars[i]);
+}
