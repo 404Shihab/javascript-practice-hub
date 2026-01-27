48d5bdc21dd44e4380aee23d35038953444d6ccb
404Shihab <shihabbhuiyan500@gmail.com>
2026-01-27 12:15:01 +0600

    Break and continue

---
+++ loop/break_continue.js
@@ -0,0 +1,16 @@
+//----------Break------------
+for(let i=0; i<=100; i++)
+{
+    if(i==50)
+        break; //the loop terminates immediately
+    console.log(i)
+}
+
+//----------Continue------------
+
+for(let i=0; i<=10; i++)
+{
+    if(i==5)
+        continue; //skips the current iteration
+    console.log(i)
+}
