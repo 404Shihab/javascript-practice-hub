5ec7faaad3826f69038bba64305c6b45a7361da0
404Shihab <shihabbhuiyan500@gmail.com>
2026-01-27 12:23:24 +0600

    do while loop

---
+++ loop/do_while.js
@@ -0,0 +1,15 @@
+let i=0;
+do{
+    console.log(i);
+    i++;
+}
+while(i<5);
+
+console.log("========================");
+
+let savings =0;
+do 
+{
+  savings+=10;    
+  console.log("Savings: " +savings);
+} while(savings <30); 
