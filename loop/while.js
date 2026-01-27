97bfd99c351f2ecbc5c0baaa38062395b29b86c2
404Shihab <shihabbhuiyan500@gmail.com>
2026-01-27 11:46:49 +0600

    while loop and file organized

---
+++ loop/while.js
@@ -0,0 +1,18 @@
+let num = 0 //loop variable
+while(num<5)
+{
+    console.log("choltesi nijer moto ", num);
+    num++;
+}
+
+console.log("-----------------------------------------");
+
+let i=1;
+let mul=7;
+
+while(i<=100)
+{
+    let ans=mul*i;
+    console.log(mul,"X",i,"=",ans);
+    i++;
+}
