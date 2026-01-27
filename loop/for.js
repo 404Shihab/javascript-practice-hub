c05a66e4f46e7a4d8cdf619c74fa3fbd63689fba
404Shihab <shihabbhuiyan500@gmail.com>
2026-01-27 12:07:06 +0600

    for loop

--- loop/for.js
+++ loop/for.js
@@ -11,3 +11,16 @@ for(let i=0; i<len; i++)
 {
     console.log(cars[i]);
 }
+
+console.log("------------------------");
+
+let sum=0;
+for(let i=1; i<=100; i++)
+{
+    if(i%2!=0)
+    {
+        console.log(i);
+        sum += i;
+    }
+}
+console.log('sum',sum);
