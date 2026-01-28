7b665aaddcec6974ff503fd25ec140f8dddc5f9e
404Shihab <shihabbhuiyan500@gmail.com>
2026-01-28 23:20:21 +0600

    js string

---
+++ String/string.js
@@ -0,0 +1,20 @@
+const country = 'Bangladesh';
+const division = "Chattagram";
+const district = `Cumilla`;
+const thana = new String('Titas');
+
+console.log(thana);
+
+console.log("------------------");
+
+const capital = 'Dha ka';
+
+console.log(capital.length); // 6 (including space)
+console.log(capital[2]); //a
+console.log(capital[3]); // 
+console.log(capital[7]); //undefined
+
+console[1]='f';
+console.log(capital); //Dha ka
+// string is immutable -> not changeable
+// array is mutable -> changeable
