f51e2e8889ff5afa846674feb404db242e4701d0
404Shihab <shihabbhuiyan500@gmail.com>
2026-01-28 23:26:59 +0600

    concat method

--- Array/methods.js
+++ Array/methods.js
@@ -35,3 +35,56 @@ console.log(Array.isArray([45,87,56])); //true
 console.log(Array.isArray('[]')); //false
 
 // concat , slice , join
+
+
+
+let firstName = "Shihab";
+let lastName = "Bhuiyan";
+
+// Use concat() to join strings
+let fullName = firstName.concat(" ", lastName);
+
+
+console.log("Full Name:", fullName);
+
+// array concat
+
+
+let numbers1 = [1, 2, 3];
+
+
+let numbers2 = [4, 5, 6];
+
+// Use concat() to merge two arrays
+let mergedArray = numbers1.concat(numbers2);
+
+
+console.log("Merged Array:", mergedArray);
+
+
+// MULTIPLE ARRAY CONCAT EXAMPLE
+
+
+// Declare another array
+let numbers3 = [7, 8, 9];
+
+// Merge multiple arrays at once
+let allNumbers = numbers1.concat(numbers2, numbers3);
+
+// Print the final array
+console.log("All Numbers:", allNumbers);
+
+
+
+// concat() does NOT change the original array
+console.log("Original numbers1:", numbers1);
+
+
+
+// Declare a greeting string
+let greeting = "Hello";
+
+// Concatenate multiple strings
+let message = greeting.concat(", ", fullName, "!Welcome to JavaScript.");
+
+console.log(message);
