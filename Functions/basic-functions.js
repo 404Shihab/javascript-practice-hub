dee853b4166ee403e2322ea39c0a0cc6383b3f3d
404Shihab <shihabbhuiyan500@gmail.com>
2026-02-04 21:49:56 +0600

    Function

---
+++ Functions/basic-functions.js
@@ -0,0 +1,31 @@
+// Basic Functions in JavaScript
+
+// Function without parameters
+function print() 
+{
+    console.log('Hello World');
+}
+
+// Function call
+print();
+
+
+// Function with parameters and return value
+
+function sum(a, b) 
+{
+    return a + b;
+}
+
+const s = sum(4555, 1);
+console.log("Sum:", s);
+
+
+
+
+function sub(x, y) {
+    console.log("Subtract:", x - y);
+}
+
+// Function call
+sub(12, 78);
